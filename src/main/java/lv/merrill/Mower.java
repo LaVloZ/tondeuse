6246af3b77dbe22395687b309e5c4f664ab3d0e4
Abdessamed MANSOURI <mansouri.abdessamed@gmail.com>
2023-01-20 11:32:26 +0100

    refacto

--- src/main/java/lv/merrill/Mower.java
+++ src/main/java/lv/merrill/Mower.java
@@ -2,19 +2,19 @@ package lv.merrill;
 
 public class Mower {
 
-    public static Position execute(int i, int y, String direction, String instructions) {
+    public static Position execute(int x, int y, String direction, String instructions) {
         if ("N".equals(direction) && "D".equals(instructions)) {
-            return new Position(i, y, "E");
+            return new Position(x, y, "E");
         }
         if("E".equals(direction)){
-            return new Position(i, y, "N");
+            return new Position(x, y, "N");
         }
         if("S".equals(direction)){
-            return new Position(i, y, "E");
+            return new Position(x, y, "E");
         }
         if("W".equals(direction)){
-            return new Position(i, y, "S");
+            return new Position(x, y, "S");
         }
-        return new Position(i, y, "W");
+        return new Position(x, y, "W");
     }
 }
