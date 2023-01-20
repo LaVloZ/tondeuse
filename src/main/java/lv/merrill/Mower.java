9e1e6ae692bc33954e2f5c8a3939d1eff986b977
Abdessamed MANSOURI <mansouri.abdessamed@gmail.com>
2023-01-20 14:35:05 +0100

    refacto

--- src/main/java/lv/merrill/Mower.java
+++ src/main/java/lv/merrill/Mower.java
@@ -2,50 +2,46 @@ package lv.merrill;
 
 public class Mower {
 
-    private int x;
-    private int y;
-    private String direction;
+    private Position position;
 
     public Mower(Position position) {
-        this.x = position.x();
-        this.y = position.y();
-        this.direction = position.direction();
+        this.position = position;
     }
 
     public Position execute(String instruction) {
-        if("N".equals(direction) && "A".equals(instruction)) {
-            return new Position(x, y + 1, "N");
+        if("N".equals(position.direction()) && "A".equals(instruction)) {
+            return new Position(position.x(), position.y() + 1, "N");
         }
-        if("E".equals(direction) && "A".equals(instruction)) {
-            return new Position(x + 1, y, "E");
+        if("E".equals(position.direction()) && "A".equals(instruction)) {
+            return new Position(position.x() + 1, position.y(), "E");
         }
-        if("S".equals(direction) && "A".equals(instruction)) {
-            return new Position(x, y - 1, "S");
+        if("S".equals(position.direction()) && "A".equals(instruction)) {
+            return new Position(position.x(), position.y() - 1, "S");
         }
-        if("W".equals(direction) && "A".equals(instruction)) {
-            return new Position(x - 1, y, "W");
+        if("W".equals(position.direction()) && "A".equals(instruction)) {
+            return new Position(position.x() - 1, position.y(), "W");
         }
-        if("N".equals(direction) && "D".equals(instruction)) {
-            return new Position(x, y, "E");
+        if("N".equals(position.direction()) && "D".equals(instruction)) {
+            return new Position(position.x(), position.y(), "E");
         }
-        if ("E".equals(direction) && "D".equals(instruction)) {
-            return new Position(x, y, "S");
+        if ("E".equals(position.direction()) && "D".equals(instruction)) {
+            return new Position(position.x(), position.y(), "S");
         }
-        if ("S".equals(direction) && "D".equals(instruction)) {
-            return new Position(x, y, "W");
+        if ("S".equals(position.direction()) && "D".equals(instruction)) {
+            return new Position(position.x(), position.y(), "W");
         }
-        if ("W".equals(direction) && "D".equals(instruction)) {
-            return new Position(x, y, "N");
+        if ("W".equals(position.direction()) && "D".equals(instruction)) {
+            return new Position(position.x(), position.y(), "N");
         }
-        if("E".equals(direction)){
-            return new Position(x, y, "N");
+        if("E".equals(position.direction())){
+            return new Position(position.x(), position.y(), "N");
         }
-        if("S".equals(direction)){
-            return new Position(x, y, "E");
+        if("S".equals(position.direction())){
+            return new Position(position.x(), position.y(), "E");
         }
-        if("W".equals(direction)){
-            return new Position(x, y, "S");
+        if("W".equals(position.direction())){
+            return new Position(position.x(), position.y(), "S");
         }
-        return new Position(x, y, "W");
+        return new Position(position.x(), position.y(), "W");
     }
 }
