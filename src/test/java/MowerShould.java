35ce7c3428e6158ea85f17c312ab5f93fef2d844
Abdessamed MANSOURI <mansouri.abdessamed@gmail.com>
2023-01-20 11:28:42 +0100

    refacto test parametré

--- src/test/java/MowerShould.java
+++ src/test/java/MowerShould.java
@@ -2,42 +2,23 @@ import lv.merrill.Mower;
 import lv.merrill.Position;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class MowerShould {
 
-    @Test
-    public void turn_left_to_west_from_north() {
-        Position position = Mower.execute(0, 0, "N", "G");
+    @ParameterizedTest
+    @CsvSource({
+            "N, W",
+            "W, S",
+            "S, E",
+            "E, N",
+    })
+    public void turn_left(String initialDirection, String expectedDirection) {
+        Position position = Mower.execute(0, 0, initialDirection, "G");
 
         Assertions.assertThat(position.x()).isEqualTo(0);
         Assertions.assertThat(position.y()).isEqualTo(0);
-        Assertions.assertThat(position.direction()).isEqualTo("W");
-    }
-
-    @Test
-    public void turn_left_to_south_from_west() {
-        Position position = Mower.execute(0, 0, "W", "G");
-
-        Assertions.assertThat(position.x()).isEqualTo(0);
-        Assertions.assertThat(position.y()).isEqualTo(0);
-        Assertions.assertThat(position.direction()).isEqualTo("S");
-    }
-
-    @Test
-    public void turn_left_to_est_from_south() {
-        Position position = Mower.execute(0, 0, "S", "G");
-
-        Assertions.assertThat(position.x()).isEqualTo(0);
-        Assertions.assertThat(position.y()).isEqualTo(0);
-        Assertions.assertThat(position.direction()).isEqualTo("E");
-    }
-
-    @Test
-    public void turn_left_to_north_from_est() {
-        Position position = Mower.execute(0, 0, "E", "G");
-
-        Assertions.assertThat(position.x()).isEqualTo(0);
-        Assertions.assertThat(position.y()).isEqualTo(0);
-        Assertions.assertThat(position.direction()).isEqualTo("N");
+        Assertions.assertThat(position.direction()).isEqualTo(expectedDirection);
     }
 }
