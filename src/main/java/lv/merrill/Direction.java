f0a986e908fddab5a13dc21c353b664a34180ac6
Abdessamed MANSOURI <mansouri.abdessamed@gmail.com>
2023-01-23 12:07:45 +0100

    refacto

---
+++ src/main/java/lv/merrill/Direction.java
@@ -0,0 +1,57 @@
+package lv.merrill;
+
+public enum Direction {
+    NORTH("N") {
+        @Override
+        public Direction left() {
+            return null;
+        }
+
+        @Override
+        public Direction right() {
+            return null;
+        }
+    },
+    EAST("E") {
+        @Override
+        public Direction left() {
+            return null;
+        }
+
+        @Override
+        public Direction right() {
+            return null;
+        }
+    },
+    SOUTH("S") {
+        @Override
+        public Direction left() {
+            return null;
+        }
+
+        @Override
+        public Direction right() {
+            return null;
+        }
+    },
+    WEST("S") {
+        @Override
+        public Direction left() {
+            return null;
+        }
+
+        @Override
+        public Direction right() {
+            return null;
+        }
+    };
+
+    Direction(String code) {
+        this.code = code;
+    }
+
+    private String code;
+
+    public abstract Direction left();
+    public abstract Direction right();
+}
--- src/main/java/lv/merrill/Position.java
+++ src/main/java/lv/merrill/Position.java
@@ -1,4 +1,56 @@
 package lv.merrill;
 
-public record Position(Coordinate coordinate, String direction) {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class Position {
+
+    private static Map<String, Direction> directions = new HashMap<>();
+    static {
+        directions.put("N", Direction.NORTH);
+        directions.put("E", Direction.EAST);
+        directions.put("S", Direction.SOUTH);
+        directions.put("W", Direction.WEST);
+    }
+    private Coordinate coordinate;
+    private String direction;
+
+    public Position(Coordinate coordinate, Direction direction) {
+        this.coordinate = coordinate;
+    }
+
+    public Position(Coordinate coordinate, String direction) {
+        this.coordinate = coordinate;
+        this.direction = direction;
+    }
+
+    public Coordinate coordinate() {
+        return coordinate;
+    }
+
+    public String direction() {
+        return direction;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return coordinate.equals(position.coordinate) && direction.equals(position.direction);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(coordinate, direction);
+    }
+
+    @Override
+    public String toString() {
+        return "Position{" +
+                "coordinate=" + coordinate +
+                ", direction=" + direction +
+                '}';
+    }
 }
