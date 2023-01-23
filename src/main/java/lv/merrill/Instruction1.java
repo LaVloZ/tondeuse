94cef758454aacdc051b29c4ba7e7c06b5d5c94e
Abdessamed MANSOURI <mansouri.abdessamed@gmail.com>
2023-01-23 14:30:39 +0100

    refacto

--- src/main/java/lv/merrill/Instruction1.java
+++ src/main/java/lv/merrill/Instruction1.java
@@ -1,13 +1,20 @@
 package lv.merrill;
 
-public record Instruction1(String instruction) implements Instruction {
+public class Instruction1 implements Instruction {
+
+    private final String instruction;
+
+    public Instruction1(String instruction) {
+        this.instruction = instruction;
+    }
+
     @Override
     public Position execute(Position position, Lawn lawn) {
-        if("A".equals(instruction())) {
+        if("A".equals(instruction)) {
             return lawn.nextPosition(position);
         }
 
-        if ("D".equals(instruction())) {
+        if ("D".equals(instruction)) {
             return position.turnRight();
         }
         return position.turnLeft();
