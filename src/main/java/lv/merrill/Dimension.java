43cc0e489866f43e0f91318ec1833605e1ac8e3f
Abdessamed MANSOURI <mansouri.abdessamed@gmail.com>
2023-01-23 15:20:53 +0100

    refacto

--- src/main/java/lv/merrill/Dimension.java
+++ src/main/java/lv/merrill/Dimension.java
@@ -1,4 +1,8 @@
 package lv.merrill;
 
 public record Dimension(int width, int height) {
+
+    public boolean isInside(Position nextPosition) {
+        return nextPosition.coordinate().x() < 0 || nextPosition.coordinate().y() < 0 || nextPosition.coordinate().y() >= height || nextPosition.coordinate().x() >= width;
+    }
 }
--- src/main/java/lv/merrill/Lawn.java
+++ src/main/java/lv/merrill/Lawn.java
@@ -10,18 +10,10 @@ public class Lawn {
 
     Position nextPosition(Position position) {
         Position nextPosition = position.forward();
-        if (nextPosition.coordinate().y() >= dimension.height()){
-            return position;
-        }
-        if (nextPosition.coordinate().x() >= dimension.width()){
-            return position;
-        }
-        if (nextPosition.coordinate().y() < 0){
-            return position;
-        }
-        if (nextPosition.coordinate().x() < 0){
+        if (dimension.isInside(nextPosition)){
             return position;
         }
         return nextPosition;
     }
+
 }
